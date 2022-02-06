810514650996b9c61b0c4448e45450f811751cd8
NickPellegrino7 <7npelleg@gmail.com>
2022-02-06 09:08:53 -0500

    New Judge Scene

--- AsGoodAsItGets/Assets/Scripts/JudgeScript.cs
+++ AsGoodAsItGets/Assets/Scripts/JudgeScript.cs
@@ -1,28 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JudgeScript : MonoBehaviour
 {
-    public GameObject butBackPrefab;
     public GameObject getBackPrefab;
+    public GameObject butBackPrefab;
 
     public CardsPile GetCardDeck;
     public CardsPile ButCardDeck;
 
-    int[] getCardIDs = new int[8];
-    int[] butCardIDs = new int[8];
+    public CardsPile GetDisplay;
+    public CardsPile ButDisplay;
+
+    public GameObject ChooseWinner;
+
+    public int numSubmissions = 7;
+
+    private Card[] getCards = new Card[7];
+    private Card[] butCards = new Card[7];
+    private bool[] seenCombo = new bool[7];
+
+    public Card currentGet;
+    public Card currentBut;
 
     int currentSetIndex = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-      for(int i = 1; i < 9; i ++){
-        int getID = PlayerPrefs.GetInt("ChosenGet" + i.ToString(), -1);
-        int butID = PlayerPrefs.GetInt("ChosenBut" + i.ToString(), -1);
-        getCardIDs[i - 1] = getID;
-        butCardIDs[i - 1] = butID;
+      PlayerPrefs.SetInt("ToMainScene", 1);
+      numSubmissions = 5; // for testing
+      for(int i = 1; i < (numSubmissions+1); i ++){
+        //int getID = PlayerPrefs.GetInt("ChosenGet" + i.ToString(), -1);
+        //int butID = PlayerPrefs.GetInt("ChosenBut" + i.ToString(), -1);
+
+        // For Testing:
+        int getID = i;
+        int butID = i;
 
         Card butCard = Instantiate(butBackPrefab).GetComponent<Card>();
         butCard.Initialize(butID);
@@ -31,8 +47,11 @@ public class JudgeScript : MonoBehaviour
         Card getCard = Instantiate(getBackPrefab).GetComponent<Card>();
         getCard.Initialize(getID);
         GetCardDeck.Add(getCard, false);
-      }
 
+        getCards[i - 1] = getCard;
+        butCards[i - 1] = butCard;
+      }
+      CardSwitcheroo();
 
     }
 
@@ -42,15 +61,58 @@ public class JudgeScript : MonoBehaviour
 
     }
 
+    public void WinnerChosen() {
+        print("Winner is..." + currentSetIndex.ToString());
+    }
+
+    private void CardSwitcheroo(){
+      if (currentGet) {
+              GetDisplay.Remove(currentGet);
+              currentGet.Flip();
+              GetCardDeck.Add(currentGet, true);
+          }
+      if (currentBut) {
+          ButDisplay.Remove(currentBut);
+          currentBut.Flip();
+          ButCardDeck.Add(currentBut, true);
+      }
+      currentGet = getCards[currentSetIndex];
+      currentBut = butCards[currentSetIndex];
+      seenCombo[currentSetIndex] = true;
+      GetCardDeck.Remove(currentGet);
+      ButCardDeck.Remove(currentBut);
+      GetDisplay.Add(currentGet, true);
+      ButDisplay.Add(currentBut, true);
+      currentGet.Flip();
+      currentBut.Flip();
+      if (!ChooseWinner.GetComponent<Button>().interactable) {
+          bool seenAllCombos = true;
+          for (int i = 0; i < numSubmissions; i++) {
+              if (!seenCombo[i]) {
+                  seenAllCombos = false;
+              }
+          }
+          if (seenAllCombos) {
+              ChooseWinner.GetComponent<Button>().interactable = true;
+          }
+      }
+    }
+
     public void MoveCardsForward()
     {
         currentSetIndex++;
-
+        if (currentSetIndex > (numSubmissions-1)) {
+            currentSetIndex = 0;
+        }
+        CardSwitcheroo();
     }
 
     public void MoveCardsBackward()
     {
         currentSetIndex--;
-
+        if (currentSetIndex < 0) {
+            currentSetIndex = (numSubmissions-1);
+        }
+        CardSwitcheroo();
     }
 }
